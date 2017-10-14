bcc38d684d0ad5ced0fdac3d6a7c2ae3256c23c8
Min Zeng <minzeng@mins-mbp.resource.campus.njit.edu>
2017-10-13 20:49:18 -0400

    Initial Commit

---
+++ NJIT-SwarmTests/NJIT_SwarmTests.swift
@@ -0,0 +1,36 @@
+//
+//  NJIT_SwarmTests.swift
+//  NJIT-SwarmTests
+//
+//  Created by Min Zeng on 13/10/2017.
+//  Copyright © 2017 Team4. All rights reserved.
+//
+
+import XCTest
+@testable import NJIT_Swarm
+
+class NJIT_SwarmTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
