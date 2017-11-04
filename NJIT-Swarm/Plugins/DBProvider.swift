c2a20ca0bd6555bbf788feb467207946c618285d
Min Zeng <mz299@njit.edu>
2017-11-04 18:25:20 -0400

    Fix bug

--- NJIT-Swarm/Plugins/DBProvider.swift
+++ NJIT-Swarm/Plugins/DBProvider.swift
@@ -68,7 +68,8 @@ class DBProvider {
      Like [uid1: [username: min, email: min@mail.com, ... ], uid2: [username: asha, email: asha@mail.com, ...], ...]
      */
     func getUsers(withKey: String, value: Any, dataHandler: DataHandler?) {
-        userRef.queryOrdered(byChild: withKey).queryEqual(toValue: value, childKey: withKey).observeSingleEvent(of: .value) { (snapshot) in
+        userRef.queryOrdered(byChild: withKey).queryEqual(toValue: value).observeSingleEvent(of: .value) { (snapshot) in
+            print(snapshot)
             if let value = snapshot.value {
                 if let data = value as? [String: Any] {
                     dataHandler?(data)
@@ -83,6 +84,7 @@ class DBProvider {
     
     func getUsers(withValue: Any, dataHandler: DataHandler?) {
         userRef.queryOrderedByKey().queryEqual(toValue: withValue).observeSingleEvent(of: .value) { (snapshot) in
+            print(snapshot)
             if let value = snapshot.value {
                 if let data = value as? [String: Any] {
                     dataHandler?(data)
